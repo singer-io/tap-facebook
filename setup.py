bfe76672c59f60b7dba53eac840f6da474148d56
Mike DeLaurentis <miked@stitchdata.com>
2017-06-14 09:22:30 -0400

    Version numbers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='0.1.5',
+      version='0.1.6',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
@@ -15,7 +15,7 @@ setup(name='tap-facebook',
           'facebookads==2.8.1',
           'pendulum==1.2.0',
           'requests==2.12.4',
-          'singer-python==1.6.0a4',
+          'singer-python==1.6.0',
       ],
       entry_points='''
           [console_scripts]
