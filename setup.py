361a0d56cc50338749af226bc194dc788c0fdf1f
Kyle Allan <kyle@stitchdata.com>
2017-08-30 16:14:53 -0400

    bump to singer-python 3.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='tap-facebook',
           'facebookads==2.10.1',
           'pendulum==1.2.0',
           'requests==2.12.4',
-          'singer-python==3.1.0',
+          'singer-python==3.2.0',
       ],
       entry_points='''
           [console_scripts]
