562ae808ae683f471c74bd5d215ad497871a9f41
John Miller <jmiller@rjmetrics.com>
2017-10-27 09:33:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.0.8',
+      version='1.0.9',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
