29fb112d346837a9c532ccccf108615b6112383a
Nick McCoy <nick@stitchdata.com>
2018-01-22 16:53:39 -0500

    bumping version to 1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.2.0',
+      version='1.3.0',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
