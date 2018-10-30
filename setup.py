7d401af04d6c08cf4434aaf6cdeaac6ab0251c16
Nick McCoy <nick@stitchdata.com>
2018-10-30 19:53:25 +0000

    bump facebook_business dependency to v3.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='tap-facebook',
       install_requires=[
           'attrs==16.3.0',
           'backoff==1.3.2',
-          'facebook_business==3.0.5',
+          'facebook_business==3.2.0',
           'pendulum==1.2.0',
           'requests==2.12.4',
           'singer-python==5.0.2',
