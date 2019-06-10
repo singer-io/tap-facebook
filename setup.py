95fd2a309bc30b019302a8deb870df0d07663a34
Diego Fernandez <aiguo.fernandez@gmail.com>
2019-06-10 16:27:22 -0600

    Version 1.5.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.5.11',
+      version='1.5.12',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
