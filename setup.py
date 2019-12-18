1e1d5e7171941b7fdbe222f416158cef6bcbc10e
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2019-12-18 16:11:42 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -2,39 +2,33 @@
 
 from setuptools import setup
 
-setup(name='tap-facebook',
-      version='1.8.1',
-      description='Singer.io tap for extracting data from the Facebook Ads API',
-      author='Stitch',
-      url='https://singer.io',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_facebook'],
-      install_requires=[
-          'attrs==16.3.0',
-          'backoff==1.8.0',
-          'pendulum==1.2.0',
-          'facebook_business==4.0.5',
-          'requests==2.20.0',
-          'singer-python==5.8.1',
-      ],
-      extras_require={
-          'dev': [
-              'pylint',
-              'ipdb',
-              'nose',
-          ]
-      },
-      entry_points='''
+setup(
+    name="tap-facebook",
+    version="1.8.1",
+    description="Singer.io tap for extracting data from the Facebook Ads API",
+    author="Stitch",
+    url="https://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_facebook"],
+    install_requires=[
+        "attrs==16.3.0",
+        "backoff==1.10.0",
+        "pendulum==1.2.0",
+        "facebook_business==4.0.5",
+        "requests==2.22.0",
+        "singer-python==5.8.1",
+    ],
+    extras_require={"dev": ["pylint", "ipdb", "nose"]},
+    entry_points="""
           [console_scripts]
           tap-facebook=tap_facebook:main
-      ''',
-      packages=['tap_facebook'],
-      package_data = {
-          'tap_facebook/schemas': [
-              # add schema.json filenames here
-          ],
-          'tap_facebook/schemas/shared': [
-          ]
-      },
-      include_package_data=True,
+      """,
+    packages=["tap_facebook"],
+    package_data={
+        "tap_facebook/schemas": [
+            # add schema.json filenames here
+        ],
+        "tap_facebook/schemas/shared": [],
+    },
+    include_package_data=True,
 )
