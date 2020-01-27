b38446035e73681ba48fa5b2a5969e50f19fcf9b
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2020-01-27 12:57:54 +0100

    use better naming in utils.py

--- tap_facebook/utils.py
+++ tap_facebook/utils.py
@@ -3,10 +3,10 @@ from os import path
 from singer import utils
 
 
-def get_abs_path(path):
-    return path.join(path.dirname(path.realpath(__file__)), path)
+def get_abs_path(filepath):
+    return path.join(path.dirname(path.realpath(__file__)), filepath)
 
 
-def load_schema(stream):
-    path = get_abs_path("schemas/{}.json".format(stream.name))
+def load_schema(stream_name):
+    path = get_abs_path("schemas/{}.json".format(stream_name))
     return utils.load_json(path)
