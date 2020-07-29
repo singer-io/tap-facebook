a591e78911ae26ac4943f3713d6899b4875db938
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2020-07-29 16:04:10 -0400

    Version 1.9.2 and changelog (#101)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.9.1',
+      version='1.9.2',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
