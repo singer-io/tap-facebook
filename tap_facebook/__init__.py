4df7c555270a7e1e1a8bfc0a7ef316f8b363b802
Dan Mosora <dan@stitchdata.com>
2020-07-29 17:21:43 +0000

    Remove old workaround

--- tap_facebook/__init__.py
+++ tap_facebook/__init__.py
@@ -110,22 +110,8 @@ def iter_delivery_info_filter(stream_type):
         yield filt
 
 def retry_pattern(backoff_type, exception, **wait_gen_kwargs):
-    # HACK: Workaround added due to bug with Facebook prematurely deprecating 'relevance_score'
-    # Issue being tracked here: https://developers.facebook.com/support/bugs/2489592517771422
-    def is_relevance_score(exception):
-        if getattr(exception, "body", None):
-            return exception.body().get("error", {}).get("message") == '(#100) relevance_score is not valid for fields param. please check https://developers.facebook.com/docs/marketing-api/reference/ads-insights/ for all valid values'
-        else:
-            return False
-
     def log_retry_attempt(details):
         _, exception, _ = sys.exc_info()
-        if is_relevance_score(exception):
-            raise Exception("Due to a bug with Facebook prematurely deprecating 'relevance_score' that is "
-                            "not affecting all tap-facebook users in the same way, you need to "
-                            "deselect `relevance_score` from your Insights export. For further "
-                            "information, please see this Facebook bug report thread: "
-                            "https://developers.facebook.com/support/bugs/2489592517771422") from exception
         LOGGER.info(exception)
         LOGGER.info('Caught retryable error after %s tries. Waiting %s more seconds then retrying...',
                     details["tries"],
@@ -133,7 +119,7 @@ def retry_pattern(backoff_type, exception, **wait_gen_kwargs):
 
     def should_retry_api_error(exception):
         if isinstance(exception, FacebookRequestError):
-            return exception.api_transient_error() or exception.api_error_subcode() == 99 or is_relevance_score(exception)
+            return exception.api_transient_error() or exception.api_error_subcode() == 99
         elif isinstance(exception, InsightsJobTimeout):
             return True
         return False
