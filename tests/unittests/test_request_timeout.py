9995a770805d84a41d566e729ec8e63634b2c928
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2021-10-18 21:39:31 +0530

    TDL-15863: Added unit tests

--- tests/unittests/test_request_timeout.py
+++ tests/unittests/test_request_timeout.py
@@ -47,6 +47,35 @@ class TestRequestTimeoutBackoff(unittest.TestCase):
         # verify get_ads() is called 5 times as max 5 reties provided for function
         self.assertEquals(mocked_account.get_ads.call_count, 5)
 
+    @mock.patch("pendulum.parse")
+    def test_ad_prepare_record(self, mocked_parse, mocked_sleep):
+        """ 
+            __iter__ of Ads calls a function _iterate which calls a nested prepare_record function.
+            Prepare_record calls a `facebook_business` method,`ad.api_get()`, to get a ad fields. 
+            We mock this method to raise a `Timeout` and expect the tap to retry this that function up to 5 times,
+            which is the current hard coded `max_tries` value.
+        """
+        # Mock ad object
+        mocked_ad = Mock()
+        mocked_ad.api_get = Mock()
+        mocked_ad.__getitem__ = Mock()
+        mocked_ad.api_get.side_effect = Timeout
+
+        # # Mock get_ads function return mocked ad object
+        mocked_account = Mock()
+        mocked_account.get_ads = Mock()
+        mocked_account.get_ads.side_effect = [[mocked_ad]]
+        # mocked_account.get_ads.side_effect = [[MockedEntity()]]
+
+        # Iterate ads object which calls prepare_record() inside and verify Timeout is raised
+        ad_object = Ads('', mocked_account, '', '', '')
+        with self.assertRaises(Timeout):
+            for message in ad_object:
+                pass
+
+        # verify prepare_record() function by checking call count of mocked ad.api_get()
+        self.assertEquals(mocked_ad.api_get.call_count, 5)
+
     def test__call_get_ad_sets(self, mocked_sleep):
         """ 
             AdSets._call_get_ad_sets calls a `facebook_business` method,`get_ad_sets()`, to get a batch of adsets. 
@@ -67,6 +96,35 @@ class TestRequestTimeoutBackoff(unittest.TestCase):
         # verify get_ad_sets() is called 5 times as max 5 reties provided for function
         self.assertEquals(mocked_account.get_ad_sets.call_count, 5)
 
+    @mock.patch("pendulum.parse")
+    def test_adset_prepare_record(self, mocked_parse, mocked_sleep):
+        """ 
+            __iter__ of AdSets calls a function _iterate which calls a nested prepare_record function.
+            Prepare_record calls a `facebook_business` method,`ad.api_get()`, to get a ad fields. 
+            We mock this method to raise a `Timeout` and expect the tap to retry this that function up to 5 times,
+            which is the current hard coded `max_tries` value.
+        """
+
+        # Mock adset object
+        mocked_adset = Mock()
+        mocked_adset.api_get = Mock()
+        mocked_adset.__getitem__ = Mock()
+        mocked_adset.api_get.side_effect = Timeout
+
+        # Mock get_ad_sets function return mocked ad object
+        mocked_account = Mock()
+        mocked_account.get_ad_sets = Mock()
+        mocked_account.get_ad_sets.side_effect = [[mocked_adset]]
+
+        # Iterate adset object which calls prepare_record() inside and verify Timeout is raised
+        ad_set_object = AdSets('', mocked_account, '', '', '')
+        with self.assertRaises(Timeout):
+            for message in ad_set_object:
+                pass
+
+        # verify prepare_record() function by checking call count of mocked ad.api_get()
+        self.assertEquals(mocked_adset.api_get.call_count, 5)
+
     def test__call_get_campaigns(self, mocked_sleep):
         """ 
             Campaigns._call_get_campaigns calls a `facebook_business` method,`get_campaigns()`, to get a batch of campaigns. 
@@ -86,6 +144,35 @@ class TestRequestTimeoutBackoff(unittest.TestCase):
 
         # verify get_campaigns() is called 5 times as max 5 reties provided for function
         self.assertEquals(mocked_account.get_campaigns.call_count, 5)
+
+    @mock.patch("pendulum.parse")
+    def test_campaign_prepare_record(self, mocked_parse, mocked_sleep):
+        """ 
+            __iter__ of Campaigns calls a function _iterate which calls a nested prepare_record function.
+            Prepare_record calls a `facebook_business` method,`ad.api_get()`, to get a ad fields. 
+            We mock this method to raise a `Timeout` and expect the tap to retry this that function up to 5 times,
+            which is the current hard coded `max_tries` value.
+        """
+
+        # # Mock campaign object
+        mocked_campaign = Mock()
+        mocked_campaign.api_get = Mock()
+        mocked_campaign.__getitem__ = Mock()
+        mocked_campaign.api_get.side_effect = Timeout
+
+        # # Mock get_campaigns function return mocked ad object
+        mocked_account = Mock()
+        mocked_account.get_campaigns = Mock()
+        mocked_account.get_campaigns.side_effect = [[mocked_campaign]]
+
+        # Iterate campaigns object which calls prepare_record() inside and verify Timeout is raised
+        campaign_object = Campaigns('', mocked_account, '', '', '')
+        with self.assertRaises(Timeout):
+            for message in campaign_object:
+                pass
+
+        # verify prepare_record() function by checking call count of mocked ad.api_get()
+        self.assertEquals(mocked_campaign.api_get.call_count, 5)
 
     def test_run_job(self, mocked_sleep):
         """ 
