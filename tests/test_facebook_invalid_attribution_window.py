d21d8d0ba245a0f4bd8fbe35ba0e686090b5ac56
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-11-19 14:51:49 +0530

    test: test invalid attribution window

--- tests/test_facebook_invalid_attribution_window.py
+++ tests/test_facebook_invalid_attribution_window.py
@@ -28,7 +28,7 @@ class FacebookInvalidAttributionWindow(FacebookBaseTest):
         """
             Test to verify that the error is raise when passing attribution window other than 1, 7 or 28
         """
-        conn_id = connections.ensure_connection(self)
+        self.conn_id = connections.ensure_connection(self)
 
         # runner.run_check_mode(self, conn_id)
         self.assertRaisesRegex(Exception, "The attribution window must be 1, 7 or 28.", runner.run_check_job_and_check_status(self), self)
