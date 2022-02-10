e7f2c400697267c28ebb151f1d50310702246f68
Aditya Sastry <adityas@hey.com>
2022-02-10 11:19:54 -0500

    Hotfix: remove excess logging

--- tap_facebook/__init__.py
+++ tap_facebook/__init__.py
@@ -249,7 +249,8 @@ def batch_record_failure(response):
 def rest(account):
     response = account.get_insights(fields = ['ad_id'], params = {'limit': 1})
     rate_limit = json.loads(response.headers()['x-fb-ads-insights-throttle'])
-    LOGGER.info("The percentage of allocated capacity for the associated ad account_id has consumed:  %s", rate_limit['acc_id_util_pct'])
+    LOGGER.info(response.headers())
+    # LOGGER.info("The percentage of allocated capacity for the associated ad account_id has consumed:  %s", rate_limit['acc_id_util_pct'])
     if rate_limit['acc_id_util_pct'] > 70:
         LOGGER.info("Utilization over 70%, backing off until capacity below 5%")
         while(rate_limit['acc_id_util_pct'] > 5):
