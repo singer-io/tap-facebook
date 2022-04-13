86a2cc4f1eb907f93ec293b98c67490771c1e12f
manand31 <92758907+manand31@users.noreply.github.com>
2022-04-13 09:47:30 -0400

    Added test cases or attribution window (#189)
    * Added test cases or attribution window
    
    * added a decorator to skip the test

--- tests/test_facebook_attribution_window.py
+++ tests/test_facebook_attribution_window.py
@@ -6,9 +6,6 @@ from base import FacebookBaseTest
 
 class FacebookAttributionWindow(FacebookBaseTest):
 
-    # set attribution window
-    ATTRIBUTION_WINDOW = 7
-
     @staticmethod
     def name():
         return "tap_tester_facebook_attribution_window"
@@ -17,8 +14,8 @@ class FacebookAttributionWindow(FacebookBaseTest):
         """Configuration properties required for the tap."""
         return_value = {
             'account_id': os.getenv('TAP_FACEBOOK_ACCOUNT_ID'),
-            'start_date' : '2019-07-24T00:00:00Z',
-            'end_date' : '2019-07-26T00:00:00Z',
+            'start_date': self.start_date,
+            'end_date': self.end_date,
             'insights_buffer_days': str(self.ATTRIBUTION_WINDOW)
         }
         if original:
@@ -27,35 +24,50 @@ class FacebookAttributionWindow(FacebookBaseTest):
         return_value["start_date"] = self.start_date
         return return_value
 
+    # For the test ad set up in facebook ads manager we see data on April 7th, start date is based on this data
     def test_run(self):
-        self.run_test(self.ATTRIBUTION_WINDOW) # attribution window: 7
+        # attrribution window = 7
+        self.ATTRIBUTION_WINDOW = 7
+        self.start_date = '2021-04-14T00:00:00Z'
+        self.end_date = '2021-04-15T00:00:00Z'
+        self.run_test(self.ATTRIBUTION_WINDOW, self.start_date, self.end_date)
 
+        # attribution window = 28
         self.ATTRIBUTION_WINDOW = 28
-        self.run_test(self.ATTRIBUTION_WINDOW) # attribution window: 28
+        self.start_date = '2021-04-30T00:00:00Z'
+        self.end_date = '2021-05-01T00:00:00Z'
+        self.run_test(self.ATTRIBUTION_WINDOW, self.start_date, self.end_date)
 
-    def run_test(self, attr_window):
+        # attribution window = 1
+        self.ATTRIBUTION_WINDOW = 1
+        self.start_date = '2021-04-08T00:00:00Z'
+        self.end_date = '2021-04-09T00:00:00Z'
+        self.run_test(self.ATTRIBUTION_WINDOW, self.start_date, self.end_date)
+
+
+    def run_test(self, attr_window, start_date, end_date):
         """
             Test to check the attribution window
         """
 
         conn_id = connections.ensure_connection(self)
 
-        # get start date
-        start_date = self.get_properties()['start_date']
         # calculate start date with attribution window
         start_date_with_attribution_window = self.timedelta_formatted(start_date, days=-attr_window)
 
         # 'attribution window' is only supported for 'ads_insights' streams
-        expected_streams = []
+        expected_insights_streams = []
         for stream in self.expected_streams():
             if self.is_insight(stream):
-                expected_streams.append(stream)
+                expected_insights_streams.append(stream)
 
-        # Run in check mode
+        # core streams does not respect start date so will be descoped from this test
+
+        # # Run in check mode
         found_catalogs = self.run_and_verify_check_mode(conn_id)
 
         # Select only the expected streams tables
-        catalog_entries = [ce for ce in found_catalogs if ce['tap_stream_id'] in expected_streams]
+        catalog_entries = [ce for ce in found_catalogs if ce['tap_stream_id'] in expected_insights_streams]
         self.perform_and_verify_table_and_field_selection(conn_id, catalog_entries, select_all_fields=True)
 
         # Run a sync job using orchestrator
@@ -64,7 +76,7 @@ class FacebookAttributionWindow(FacebookBaseTest):
 
         expected_replication_keys = self.expected_replication_keys()
 
-        for stream in expected_streams:
+        for stream in expected_insights_streams:
             with self.subTest(stream=stream):
 
                 replication_key = next(iter(expected_replication_keys[stream]))
@@ -78,12 +90,12 @@ class FacebookAttributionWindow(FacebookBaseTest):
                 for record in records:
                     replication_key_value = record.get(replication_key)
 
-                    # Verify the sync records respect the (simulated) start date value
+                    # Verify the sync records respect the attribution window
                     self.assertGreaterEqual(self.parse_date(replication_key_value), self.parse_date(start_date_with_attribution_window),
                                             msg="The record does not respect the attribution window.")
 
-                    # verify if the record's bookmark value is between start date and (simulated) start date value
-                    if self.parse_date(start_date_with_attribution_window) <= self.parse_date(replication_key_value) < self.parse_date(start_date):
+                    # verify if the record's bookmark value is between start date and attribution window
+                    if self.parse_date(start_date_with_attribution_window) <= self.parse_date(replication_key_value) <= self.parse_date(start_date):
                         is_between = True
 
-                    self.assertTrue(is_between, msg="No record found between start date and attribution date.")
+                    self.assertTrue(is_between)
--- tests/test_facebook_invalid_attribution_window.py
+++ tests/test_facebook_invalid_attribution_window.py
@@ -1,14 +1,15 @@
 import os
+import unittest
 
 from tap_tester import runner, connections, menagerie
 
 from base import FacebookBaseTest
 
-class FacebookInvalidAttributionWindow(FacebookBaseTest):
+class FacebookInvalidAttributionWindowInt(FacebookBaseTest):
 
     @staticmethod
     def name():
-        return "tap_tester_facebook_invalid_attribution_window"
+        return "tt_facebook_invalid_window_int"
 
     def get_properties(self, original: bool = True):
         """Configuration properties required for the tap."""
@@ -16,7 +17,7 @@ class FacebookInvalidAttributionWindow(FacebookBaseTest):
             'account_id': os.getenv('TAP_FACEBOOK_ACCOUNT_ID'),
             'start_date' : '2019-07-24T00:00:00Z',
             'end_date' : '2019-07-26T00:00:00Z',
-            'insights_buffer_days': '10' # set attribution window other than 1, 7 or 28
+            'insights_buffer_days': self.ATTRIBUTION_WINDOW,
         }
         if original:
             return return_value
@@ -25,6 +26,10 @@ class FacebookInvalidAttributionWindow(FacebookBaseTest):
         return return_value
 
     def test_run(self):
+        self.ATTRIBUTION_WINDOW = '10' # set attribution window other than 1, 7 or 28
+        self.run_test()
+
+    def run_test(self):
         """
             Test to verify that the error is raise when passing attribution window other than 1, 7 or 28
         """
@@ -38,3 +43,19 @@ class FacebookInvalidAttributionWindow(FacebookBaseTest):
         discovery_error_message = exit_status.get('discovery_error_message')
         # validate the error message
         self.assertEquals(discovery_error_message, "The attribution window must be 1, 7 or 28.")
+        self.assertIsNone(exit_status.get('target_exit_status'))
+        self.assertIsNone(exit_status.get('tap_exit_status'))
+
+
+class FacebookInvalidAttributionWindowStr(FacebookInvalidAttributionWindowInt):
+
+    @staticmethod
+    def name():
+        return "tt_facebook_invalid_window_str"
+    
+    @unittest.skip("BUG: TDL-18569")
+    def test_run(self):
+        self.ATTRIBUTION_WINDOW = 'something'
+        self.run_test()
+
+    # BUG : TDL-18569 created to standarize the error message for sting values for attribution window
