c1b03fc37b992bdb94f76fe489fa4e72d3f0ebc1
Ron Assa <ron@analytika.io>
2022-10-24 19:07:23 +0300

    adding bid_strategy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='pp-1.18.6',
+      version='pp-1.18.11',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
