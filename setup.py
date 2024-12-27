a7674def7cf197fc71da0bdb9a36ef94840b51d0
Sourabh Gandhi <sgandhi@talend.com>
2024-12-27 03:28:56 +0000

    update facebook sdk

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.20.2',
+      version='1.21.0',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
@@ -12,7 +12,7 @@ setup(name='tap-facebook',
       install_requires=[
           'attrs==17.3.0',
           'backoff==2.2.1',
-          'facebook_business==19.0.2',
+          'facebook_business==21.0.5',
           'pendulum==1.2.0',
           'requests==2.20.0',
           'singer-python==6.0.0',
